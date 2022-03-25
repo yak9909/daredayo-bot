5caaa4701849a56fc0eda66f8ed72b5d4b082067
yak9909 <Mayu8680@yahoo.co.jp>
2022-03-25 11:53:58 +0900

    update yomiage.py

--- cogs/yomiage.py
+++ cogs/yomiage.py
@@ -10,11 +10,13 @@ class Yomiage(commands.Cog):
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
         channel = await self.bot.fetch_channel(921297377858572308)
 
-        if not before.channel:
-            await channel.send(f"{after.channel.mention} {member.mention} おっぱい")
-
-        if not after.channel:
-            await channel.send(f"{member.mention} ちんちん・・・")
+        if member.guild.id == 908140851442618379:
+            if after.channel:
+                if len(after.channel.members) > 0:
+                    await channel.send(f"{member.mention} が {after.channel.mention} で通話を開始しました")
+            else:
+                if len(before.channel.members) == 0:
+                    await channel.send(f"{before.channel.mention} 通話が終了しました")
 
 
 def setup(bot: commands.Bot):
